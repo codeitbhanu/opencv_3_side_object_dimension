59aebe2d63c2d0bc0467a83f593a746726bba9ce
codeitbhanu <codeitbhanu@gmail.com>
2017-04-27 07:40:28 +0530

    Blocked Rectangle Area in Second Biggest Contour

--- detect_two_contour.py
+++ detect_two_contour.py
@@ -50,6 +50,8 @@ for i, c in enumerate(contours):
     area = cv2.contourArea(c)
     areaArray.append(area)
 
+# cv2.fillPoly(overlay, contours, [104, 255, 255])
+
 # first sort the array by area
 sorteddata = sorted(zip(areaArray, contours), key=lambda x: x[0], reverse=True)
 
@@ -59,7 +61,7 @@ secondlargestcontour = sorteddata[1][1]
 # draw it
 x, y, w, h = cv2.boundingRect(secondlargestcontour)
 cv2.drawContours(im, secondlargestcontour, -1, (255, 0, 0), 2)
-cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
-cv2.rectangle(im, secondlargestcontour, cv.Scalar(255,0,0), cv.CV_FILLED);
+cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 255), -1)
+
 # rectangle_contour(im, secondlargestcontour)
 cv2.imwrite('/home/bhanu/projects/opencv/with_camera/two_contour_detection/output.jpg', im)
